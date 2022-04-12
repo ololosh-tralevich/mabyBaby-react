36e7e440fc31af588e61e48df97968db8d91601a
ololosh-tralevich <karpeko76@gmail.com>
2022-04-12 21:58:03 +0100

    upd

---
+++ src/redux/store.js
@@ -0,0 +1,38 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import userReducer from './userAccount/userAccount-slice';
+import qaReducer from './qaTests/qaTests-reducer';
+
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    user: persistedUserReducer,
+    contacts: qaReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
--- src/shared/api/qaApi/qaApi.js
+++ src/shared/api/qaApi/qaApi.js
@@ -2,33 +2,33 @@ import axios from 'axios';
 
 const getTechTest = () => {
   const data = axios.get('/qa-test/tech');
-//   console.log('Tech Test: ', data);
+  //   console.log('Tech Test: ', data);
   return data;
 };
 
 const getTheoryTest = () => {
-    const data = axios.get('/qa-test');
-    // console.log('Theory test: ', data)
-    return data;
-}
+  const data = axios.get('/qa-test');
+  // console.log('Theory test: ', data)
+  return data;
+};
 
-const getTechResult = (answersObj) => {
-    const {data: result} = axios.post('/qa-test/tech-results', answersObj);
-    // console.log('Tech results: ', result)
-    return result;
-}
+const getTechResult = answersObj => {
+  const { data: result } = axios.post('/qa-test/tech-results', answersObj);
+  // console.log('Tech results: ', result)
+  return result;
+};
 
 const getTheoryResult = answersObj => {
-    const {data: result} = axios.post('/qa-test/theory-results', answersObj);
-    // console.log('Theory results: ', result)
-    return result
-}
+  const { data: result } = axios.post('/qa-test/theory-results', answersObj);
+  // console.log('Theory results: ', result)
+  return result;
+};
 
 const qaApi = {
-    getTechTest,
-    getTheoryTest,
-    getTechResult,
-    getTheoryResult
-}
+  getTechTest,
+  getTheoryTest,
+  getTechResult,
+  getTheoryResult,
+};
 
 export default qaApi;
