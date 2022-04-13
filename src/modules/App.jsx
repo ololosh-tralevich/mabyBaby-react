fb56a6b838a5deabdb0888a1c22be5af0404b87b
ololosh-tralevich <karpeko76@gmail.com>
2022-04-13 13:01:14 +0100

    upd

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -1,41 +1,13 @@
-import { lazy, Suspense, useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 // import { useDispatch } from 'react-redux';
 // import { userOperations } from '../redux/userAccount/userAccount-operations';
-import PublicRoute from '../shared/components/PublicRoute';
-import PrivateRoute from '../shared/components/PrivateRoute';
 
-import LayoutPage from '../pages/LayoutPage';
-const AuthPage = lazy(() => import('../pages/AuthPage'));
-const MainPage = lazy(() => import('../pages/MainPage'));
-const TestPage = lazy(() => import('../pages/TestPage'));
-const ResultsPage = lazy(() => import('../pages/ResultsPage'));
-const UsefulInfoPage = lazy(() => import('../pages/UsefulInfoPage'));
-const ContactsPage = lazy(() => import('../pages/ContactsPage'));
-
-// import '../sass/main.scss';
+import Routes from './Routes';
+import '../sass/main.scss';
 
 const App = () => {
-  return (
-    <Suspense fallback={<div className="ldsDualRing"></div>}>
-      <Routes>
-        <Route path="/" element={<LayoutPage />}>
-          <Route element={<PublicRoute />}>
-            <Route path="auth" element={<AuthPage />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route index element={<MainPage />} />
-            <Route path="test" element={<TestPage />} />
-            <Route path="results" element={<ResultsPage />} />
-            <Route path="useful-info" element={<UsefulInfoPage />} />
-            <Route path="contacts" element={<ContactsPage />} />
-          </Route>
-        </Route>
-        <Route path='*' element={<Navigate to='/' replace/>}/>
-      </Routes>
-    </Suspense>
-  );
+  return <Routes />;
 };
 
-export default App
+export default App;
