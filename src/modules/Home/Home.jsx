e8961f7a798c44a10681bb07cf167925b9c3a673
ololosh-tralevich <karpeko76@gmail.com>
2022-04-14 22:51:56 +0100

    done home page

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import { useSelector, shallowEqual } from 'react-redux';
 
 import { getGlobalStore } from '../../redux/userAccount/userAccount-selectors';
+
+import ArrowRight from './ArrowRight';
 
 import styles from './home.module.scss';
 
@@ -9,10 +13,30 @@ const Home = () => {
   console.log(globalStore);
 
   return (
-    <div>
-      {/* your code */}
-      <p>main</p>
-    </div>
+    <section className="container">
+      <div className={styles.mainBlock}>
+        <h2 className={styles.firstTitle}>
+          “Regression testing. What is it?
+          <br /> If the system compiles, that's good, if it boots, that's
+          great!”
+        </h2>
+        <div className={styles.horizontalLine}></div>
+        <h3 className={styles.secondTitle}>Linus Torvalds</h3>
+        <h4 className={styles.thirdTitle}>
+          Linux kernel creator, hacker, 1969
+        </h4>
+        <div className={styles.linkBlock}>
+          <Link className={styles.testLink} to="test">
+            QA technical training
+            <ArrowRight className={styles.arrowRight} color={'white'} />
+          </Link>
+          <Link className={styles.testLink} to="test">
+            Testing theory
+            <ArrowRight className={styles.arrowRight} color={'white'} />
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
