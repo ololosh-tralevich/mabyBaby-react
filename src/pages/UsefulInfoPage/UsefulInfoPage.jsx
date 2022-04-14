3a885e781a8d542128887f634895bfbc5ab16ba7
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-04-14 14:44:48 +0300

    2

--- src/pages/UsefulInfoPage/UsefulInfoPage.jsx
+++ src/pages/UsefulInfoPage/UsefulInfoPage.jsx
@@ -1,8 +1,22 @@
-import UsefulInfo from '../../modules/UsefulInfo/UsefulInfo';
+// import UsefulInfo from '../../modules/UsefulInfo/UsefulInfo';
+
+import InfoList from "../../shared/components/InfoList";
+import { literature } from "./info-content/literature";
+import { resources } from "./info-content/resources";
+
+import s from "./info-page.module.scss";
 
 const UsefulInfoPage = () => {
-  
-  return <UsefulInfo />;
+   return (
+        <main>            
+            <div className="container">
+                <div className={s.info}>                    
+                    <InfoList infoList={literature} listTitle="Useful literature" />                  
+                    <InfoList infoList={resources} listTitle="Useful resources" />                
+                </div>                 
+            </div>           
+        </main>        
+    )
 };
 
 export default UsefulInfoPage;
