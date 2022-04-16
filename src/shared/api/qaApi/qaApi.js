295a1cb71a9050434c19c37d40bd84c56b716ddd
ololosh-tralevich <karpeko76@gmail.com>
2022-04-16 23:39:32 +0100

    upd

--- src/shared/api/qaApi/qaApi.js
+++ src/shared/api/qaApi/qaApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const getTest = async (type) => {
-  const data = await axios.get('/qa-test/'+type);  
+  const data = await axios.get(`/qa-test/${type}`);  
   return data;
 };
 
 const getResult = async ({answers, type}) => {
-  const { data: result } = await axios.post('/qa-test/'+type+'-results', {answers});  
+  const { data: result } = await axios.post(`/qa-test/${type}-results`, {answers});  
   return result;
 };
 
